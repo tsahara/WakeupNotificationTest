bce31141130b28cac99057479bbc8d760d74f8cc
Tomoyuki Sahara <sahara@caddr.net>
2017-08-18 23:38:46 +0900

    sleep and wakeup

--- WakeNotificationTest/AppDelegate.swift
+++ WakeNotificationTest/AppDelegate.swift
@@ -11,16 +11,23 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let notificationCenter = NSWorkspace.shared().notificationCenter
+        notificationCenter.addObserver(self, selector: #selector(AppDelegate.sleep), name: NSNotification.Name.NSWorkspaceWillSleep, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(AppDelegate.wakeup), name: NSNotification.Name.NSWorkspaceDidWake, object: nil)
+        print("observing...")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    func wakeup(notification: NSNotification) {
+        print("wake up!")
+    }
 
-
+    func sleep(notification: NSNotification) {
+        print("sleeping...")
+    }
 }
 
