52093ae831f88ba54663e88d8a7d6aa41b61246c
Tomoyuki Sahara <sahara@caddr.net>
2017-08-18 23:14:38 +0900

    Initial Commit

---
+++ WakeNotificationTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WakeNotificationTest
+//
+//  Created by Tomoyuki Sahara on 2017/08/18.
+//  Copyright © 2017 Tomoyuki Sahara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WakeNotificationTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WakeNotificationTest
+//
+//  Created by Tomoyuki Sahara on 2017/08/18.
+//  Copyright © 2017 Tomoyuki Sahara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
